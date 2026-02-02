8dc3e9796ae72e1d52c587bef2a0b104266f44fa
Ivan Volkov <ivolkov03@mail.ru>
2026-02-02 13:27:52 +0300

    Исправть корректировки по коду, 1 ревью

--- src/ru/yandex/practicum/exception/WordNotFoundException.java
+++ src/ru/yandex/practicum/exception/WordNotFoundException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.exception;
 
-public class WordNotFoundException extends Exception{
+public class WordNotFoundException extends Exception {
     public WordNotFoundException(String word) {
         super("Слово '" + word + "' не найдено в словаре.");
     }
--- src/ru/yandex/practicum/exception/WordleGameException.java
+++ src/ru/yandex/practicum/exception/WordleGameException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.exception;
 
-public class WordleGameException extends RuntimeException{
+public class WordleGameException extends RuntimeException {
     public WordleGameException(String message) {
         super(message);
     }
