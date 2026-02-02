564a6182da85545eb87c69acc3cfaa7f3bc0359c
Ivan Volkov <ivolkov03@mail.ru>
2026-02-02 13:26:44 +0300

    Исправть корректировки по коду, 1 ревью

--- src/ru/yandex/practicum/exception/InvalidWordLengthException.java
+++ src/ru/yandex/practicum/exception/InvalidWordLengthException.java
@@ -1,7 +1,5 @@
 package ru.yandex.practicum.exception;
 
-import ru.yandex.practicum.WordleGame;
-
 public class InvalidWordLengthException extends Exception {
     public InvalidWordLengthException() {
         super("Слово должно состоять ровно из 5 букв.");
